9ac8fff8ad8b72fe38a323babe1a40175bf1b1e6
Tigor Hutasuhut <tigor.hutasuhut@gmail.com>
2021-07-08 15:13:21 +0700

    added lazygit to toggleterm

--- no-gui/post-plugs/terminal.lua
+++ no-gui/post-plugs/terminal.lua
@@ -20,3 +20,13 @@ require("toggleterm").setup{
     direction = 'float',
     close_on_exit = true, -- close the terminal window when the process exits
 }
+
+local Terminal  = require('toggleterm.terminal').Terminal
+local lazygit = Terminal:new({ cmd = "lazygit", hidden = true })
+
+function _lazygit_toggle()
+  lazygit:toggle()
+end
+
+vim.api.nvim_set_keymap("n", "<leader>g", "<cmd>lua _lazygit_toggle()<CR>", {noremap = true, silent = true})
+
