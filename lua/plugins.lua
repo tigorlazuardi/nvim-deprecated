8b43c91b089952ea5c524edb2dcfb0dc1a3bb89e
Tigor Hutasuhut <tigor.hutasuhut@gmail.com>
2021-07-13 09:46:02 +0700

    replaced '/' with '.'

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -4,30 +4,30 @@ return require("packer").startup(function(use)
     -- Let the packer manages itself
     use "wbthomason/packer.nvim"
     -- Auto cwd to workspace root
-    require("config/rooter")(use)
-    require("config/theme")(use)
+    require("config.rooter")(use)
+    require("config.theme")(use)
 
-    require("config/autopairs")(use)
+    require("config.autopairs")(use)
 
-    require("config/kommentary")(use)
+    require("config.kommentary")(use)
 
-    require("config/explorer")(use)
+    require("config.explorer")(use)
 
-    require("config/formatter")(use)
+    require("config.formatter")(use)
 
-    require("config/git")(use)
+    require("config.git")(use)
 
-    require("config/golang")(use)
+    require("config.golang")(use)
 
-    require("config/lsp")(use)
+    require("config.lsp")(use)
 
-    require("config/status_line")(use)
+    require("config.status_line")(use)
 
-    require("config/snippets")(use)
+    require("config.snippets")(use)
 
-    require("config/todo")(use)
+    require("config.todo")(use)
 
-    require("config/telescope")(use)
+    require("config.telescope")(use)
 
     require("config.terminal")(use)
 
