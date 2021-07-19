56aa3297eb77589b0be737f4d9c5f52e1d83b4e6
Tigor Hutasuhut <tigor.hutasuhut@gmail.com>
2021-07-19 10:04:56 +0700

    IsWindows helper function

--- init.lua
+++ init.lua
@@ -1,4 +1,9 @@
-if vim.fn.has('win32') ~= 0 or vim.fn.has('win64') ~= 0 then
+---@return boolean
+function _G.IsWindows()
+    return vim.fn.has('win32') ~= 0 or vim.fn.has('win64') ~= 0
+end
+
+if _G.IsWindows() then
     _G.CONFIG_PATH = vim.fn.expand('$HOME') .. '/AppData/Local/nvim'
 else
     _G.CONFIG_PATH = vim.fn.expand('$HOME') .. '/.config/nvim'
