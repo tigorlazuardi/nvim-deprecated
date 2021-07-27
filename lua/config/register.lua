1bf102998f03bb050230b39bd5985ad07f8db7d9
Tigor Hutasuhut <tigor.hutasuhut@gmail.com>
2021-07-27 20:27:37 +0700

    using tversteeg register instead of peekup

--- lua/config/register.lua
+++ lua/config/register.lua
@@ -1,11 +1,3 @@
 return function(use)
-	use({
-		"gennaro-tedesco/nvim-peekup",
-		config = function()
-			require("nvim-peekup.config").on_keystroke["delay"] = "1ms"
-			require("nvim-peekup.config").on_keystroke["paste_reg"] = '"'
-			vim.api.nvim_set_keymap("n", "<leader>P", "<Plug>PeekupPasteBefore", {})
-			vim.api.nvim_set_keymap("n", "<leader>p", "<Plug>PeekupPasteAfter", {})
-		end,
-	})
+	use("tversteeg/registers.nvim")
 end
