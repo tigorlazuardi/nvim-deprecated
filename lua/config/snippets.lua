3d2cd8e9119806acacf5b62ee2dc376351597949
Tigor Hutasuhut <tigor.hutasuhut@gmail.com>
2021-07-27 20:34:14 +0700

    tab now compe

--- lua/config/snippets.lua
+++ lua/config/snippets.lua
@@ -1,16 +1,3 @@
-local function run()
-	vim.cmd([[
-        imap <expr> <C-j>   vsnip#expandable()  ? '<Plug>(vsnip-expand-or-jump)'         : '<C-j>'
-        smap <expr> <C-j>   vsnip#expandable()  ? '<Plug>(vsnip-expand-or-jump)'         : '<C-j>'
-
-        " Jump forward or backward
-        imap <expr> <Tab>   vsnip#jumpable(1)   ? '<Plug>(vsnip-jump-next)'      : '<Tab>'
-        smap <expr> <Tab>   vsnip#jumpable(1)   ? '<Plug>(vsnip-jump-next)'      : '<Tab>'
-        imap <expr> <S-Tab> vsnip#jumpable(-1)  ? '<Plug>(vsnip-jump-prev)'      : '<S-Tab>'
-        smap <expr> <S-Tab> vsnip#jumpable(-1)  ? '<Plug>(vsnip-jump-prev)'      : '<S-Tab>'
-    ]])
-end
-
 return function(use)
 	vim.g.vsnip_snippet_dir = vim.fn.stdpath("config") .. "/snippets/vsnip/"
 	use("rafamadriz/friendly-snippets")
