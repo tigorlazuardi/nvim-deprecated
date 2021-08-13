99f3ac67400475a75684076bf2fb45f7bd9f8026
Tigor Hutasuhut <tigor.hutasuhut@gmail.com>
2021-08-13 16:11:26 +0700

    added vim yoink

--- lua/config/register.lua
+++ lua/config/register.lua
@@ -1,3 +1,13 @@
+local function set_globals()
+	vim.g.yoinkSyncNumberedRegisters = 1
+	vim.g.yoinkSavePersistently = 1
+	vim.g.yoinkIncludeDeleteOperations = 1
+end
+
 return function(use)
-	use("tversteeg/registers.nvim")
+	set_globals()
+	use('tversteeg/registers.nvim')
+
+	-- rotates clipboard yanks to 0-9 register
+	use('svermeulen/vim-yoink')
 end
