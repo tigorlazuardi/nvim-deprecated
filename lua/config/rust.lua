ee6ce9b517b1d02ea7f7e551a7e4997cc2b4ac87
Tigor Hutasuhut <tigor.hutasuhut@gmail.com>
2021-08-14 08:27:21 +0700

    added rust analyzer

--- lua/config/rust.lua
+++ lua/config/rust.lua
@@ -1,7 +1,32 @@
 local function rust_tools_config()
-	require('rust-tools').setup({})
+	local capabilities = require('lsp.capabilities')
+	require('rust-tools').setup({
+		server = {
+			on_attach = require('lsp.on_attach'),
+			capabilities = capabilities,
+			settings = {
+				['rust-analyzer'] = {
+					assist = {
+						importGranularity = 'module',
+						importPrefix = 'by_self',
+					},
+					cargo = {
+						loadOutDirsFromCheck = true,
+					},
+					procMacro = {
+						enable = true,
+					},
+				},
+			},
+		},
+	})
 end
 
 return function(use)
-	use({ 'simrat39/rust-tools.nvim', config = rust_tools_config, ft = { 'rust' } })
+	use({
+		'simrat39/rust-tools.nvim',
+		requires = { 'neovim/nvim-lspconfig' },
+		config = rust_tools_config,
+		ft = { 'rust' },
+	})
 end
