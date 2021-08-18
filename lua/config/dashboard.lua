6ee7e70ee8e35a5da2e528a53e63bce128bf988b
Tigor Hutasuhut <tigor.hutasuhut@gmail.com>
2021-08-18 11:04:15 +0700

    feat: added dashboard custom section

--- lua/config/dashboard.lua
+++ lua/config/dashboard.lua
@@ -1,5 +1,28 @@
 return function(use)
+	local custom_section = {
+		a = {
+			description = { '  Find File          ' },
+			command = 'Telescope find_files',
+		},
+		b = {
+			description = { '  Recently Used Files' },
+			command = 'Telescope oldfiles',
+		},
+		c = {
+			description = { '  Recent Projects    ' },
+			command = 'Telescope projects',
+		},
+		d = {
+			description = { '  Load Last Session  ' },
+			command = 'SessionLoad',
+		},
+		e = {
+			description = { '  Find Word          ' },
+			command = 'Telescope live_grep',
+		},
+	}
 	vim.g.dashboard_default_executive = 'telescope'
+	vim.g.dashboard_custom_section = custom_section
 
 	use({
 		'glepnir/dashboard-nvim',
