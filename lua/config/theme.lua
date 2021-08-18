60572eee4519fbd486c59a1cedb60c333c58f942
Tigor Hutasuhut <tigor.hutasuhut@gmail.com>
2021-08-18 17:16:13 +0700

    feat: added nightfox theme

--- lua/config/theme.lua
+++ lua/config/theme.lua
@@ -1,4 +1,4 @@
-local function setglobals()
+local function tokyonight_globals()
 	vim.g.tokyonight_style = 'storm'
 	vim.g.tokyonight_italic_functions = true
 	vim.g.tokyonight_sidebars = { 'qf', 'vista_kind', 'terminal', 'packer', 'NvimTree' }
@@ -14,17 +14,29 @@ local function setglobals()
 	vim.g.edge_disable_italic_comment = 0
 end
 
+local function nightfox_globals()
+	vim.g.nightfox_italic_functions = true
+	vim.g.nightfox_italic_keywords = true
+end
+
 return function(use)
-	setglobals()
+	tokyonight_globals()
+	nightfox_globals()
 	use('maaslalani/nordbuddy')
 
 	use({
 		'folke/tokyonight.nvim',
 		config = function()
-			vim.cmd([[colorscheme tokyonight]])
+			-- vim.cmd([[colorscheme tokyonight]])
 		end,
 	})
 
 	use({ 'dracula/vim', as = 'dracula' })
+	use({
+		'EdenEast/nightfox.nvim',
+		config = function()
+			vim.cmd([[colorscheme nightfox]])
+		end,
+	})
 	use('sainnhe/edge')
 end
