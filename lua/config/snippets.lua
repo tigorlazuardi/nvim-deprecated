7ac6277087aa813d76098450cb934852ca668cc8
Tigor Hutasuhut <tigor.hutasuhut@gmail.com>
2021-08-23 15:26:56 +0700

    update(keymaps): tab will not move autocomplete selection or jump. it replaced by c-j

--- lua/config/snippets.lua
+++ lua/config/snippets.lua
@@ -1,3 +1,14 @@
+function run()
+	local option = { silent = true }
+
+	vim.api.nvim_set_keymap('i', '<c-j>', '<Plug>(vsnip-jump-next)', option)
+	vim.api.nvim_set_keymap('i', '<c-k>', '<Plug>(vsnip-jump-prev)', option)
+	-- vim.cmd([[
+	--        imap <silent> <C-j>     <Plug>(vsnip-jump-next)
+	-- 	imap <silent> <C-k>		<Plug>(vsnip-jump-prev)
+	-- ]])
+end
+
 return function(use)
 	vim.g.vsnip_snippet_dir = vim.fn.stdpath('config') .. '/snippets/vsnip/'
 	use({ 'rafamadriz/friendly-snippets', disable = vim.g.vscode })
