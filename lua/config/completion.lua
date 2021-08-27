f51c40edec7de7fecf3d035ed05389151102c5aa
Tigor Hutasuhut <tigor.hutasuhut@gmail.com>
2021-08-27 14:13:09 +0700

    fix(completion): fixed bad mapping on completion confirm

--- lua/config/completion.lua
+++ lua/config/completion.lua
@@ -58,7 +58,7 @@ local function cmp_config()
 			['<C-f>'] = cmp.mapping.scroll_docs(4),
 			['<C-Space>'] = cmp.mapping.complete(),
 			['<C-e>'] = cmp.mapping.close(),
-			['C-y'] = cmp.mapping.confirm({
+			['<C-y>'] = cmp.mapping.confirm({
 				behavior = cmp.ConfirmBehavior.Insert,
 				select = true,
 			}),
