ed37ca0aa8dcd1c2396704f54a6bc6b183a621ff
Tigor Hutasuhut <tigor.hutasuhut@gmail.com>
2021-08-27 13:30:29 +0700

    chore(cutlass): format code

--- lua/config/cutlass.lua
+++ lua/config/cutlass.lua
@@ -1,13 +1,13 @@
 return function(use)
 	use({
-		"svermeulen/vim-cutlass",
+		'svermeulen/vim-cutlass',
 		config = function()
 			local remap = vim.api.nvim_set_keymap
 			local opts = { noremap = true }
-			remap("n", "x", "d", opts)
-			remap("x", "x", "d", opts)
-			remap("n", "xx", "dd", opts)
-			remap("n", "X", "D", opts)
+			remap('n', 'x', 'd', opts)
+			remap('x', 'x', 'd', opts)
+			remap('n', 'xx', 'dd', opts)
+			remap('n', 'X', 'D', opts)
 		end,
 	})
 end
