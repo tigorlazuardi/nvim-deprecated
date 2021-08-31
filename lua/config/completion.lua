dcb07eeb99f0abbfcc06cbb64b0b3706c367a6d5
Tigor Hutasuhut <tigor.hutasuhut@gmail.com>
2021-08-31 19:26:44 +0700

    fix(completion): fix cmp logic for lsp symbols

--- lua/config/completion.lua
+++ lua/config/completion.lua
@@ -69,7 +69,7 @@ local function cmp_config()
 		},
 		formatting = {
 			format = function(entry, vim_item)
-				vim_item.kind = require('lspkind').presets.default[vim_item.kind] or ' ' .. ' ' .. vim_item.kind
+				vim_item.kind = (require('lspkind').presets.default[vim_item.kind] or ' ') .. ' ' .. vim_item.kind
 				-- set a name for each source
 				vim_item.menu = ({
 					buffer = '[Buffer]',
