62809e09c4237bc9a1b7c0ff57802b44304bd095
Tigor Hutasuhut <tigor.hutasuhut@gmail.com>
2021-09-02 08:54:49 +0700

    feature(autopairs): autopairs now check ts and does not enter completion on enter

--- lua/config/completion.lua
+++ lua/config/completion.lua
@@ -97,14 +97,32 @@ local function cmp_config()
 			{ name = 'luasnip' },
 		},
 	})
+
 	require('nvim-autopairs').setup({
-		disable_filetype = { 'TelescopePrompt', 'vim' },
+		disable_filetype = { 'TelescopePrompt' },
+		check_ts = true,
 	})
-	require('nvim-autopairs.completion.cmp').setup({
-		map_cr = true, --  map <CR> on insert mode
-		map_complete = true, -- it will auto insert `(` after select function or method item
-		auto_select = true, -- automatically select the first item
-	})
+	local remap = vim.api.nvim_set_keymap
+	local npairs = require('nvim-autopairs')
+
+	-- skip it, if you use another global object
+	_G.MUtils = {}
+
+	MUtils.completion_confirm = function()
+		if vim.fn.pumvisible() ~= 0 then
+			return npairs.esc('<cr>')
+		else
+			return npairs.autopairs_cr()
+		end
+	end
+
+	remap('i', '<CR>', 'v:lua.MUtils.completion_confirm()', { expr = true, noremap = true })
+
+	-- require('nvim-autopairs.completion.cmp').setup({
+	-- 	map_cr = true, --  map <CR> on insert mode
+	-- 	map_complete = true, -- it will auto insert `(` after select function or method item
+	-- 	auto_select = true, -- automatically select the first item
+	-- })
 
 	local opts = { silent = true }
 	vim.api.nvim_set_keymap('i', '<c-j>', '<Plug>luasnip-expand-or-jump', opts)
