c309b491b28d9fd02f1969161c34eecc525d2f86
Tigor Hutasuhut <tigor.hutasuhut@gmail.com>
2021-09-04 00:09:23 +0700

    feature(golang): added goimpl plugin `<leader>im` for filetype go

--- lua/config/golang.lua
+++ lua/config/golang.lua
@@ -5,4 +5,27 @@ return function(use)
 		config = require('lsp.go').rayxgo_setup,
 		ft = { 'go' },
 	})
+
+	use({
+		'edolphin-ydf/goimpl.nvim',
+		requires = {
+			{ 'nvim-lua/plenary.nvim' },
+			{ 'nvim-lua/popup.nvim' },
+			{ 'nvim-telescope/telescope.nvim' },
+			{ 'nvim-treesitter/nvim-treesitter' },
+		},
+		ft = {'go'},
+		config = function()
+			require('telescope').load_extension('goimpl')
+
+			local ok, wk = pcall(require, 'which-key')
+			if not ok then
+				return
+			end
+
+			wk.register({
+				['<leader>im'] = {[[<cmd>lua require'telescope'.extensions.goimpl.goimpl{}<CR>]], 'Go Impl' },
+			})
+		end,
+	})
 end
