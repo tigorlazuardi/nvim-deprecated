12c16c57a0b61848bf966422d0d8cf4cfc2dcce0
TigorHutasuhut <tigor.hutasuhut@gmail.com>
2021-09-23 09:43:53 +0700

    revert(null-ls): removed lsp.go.golangcililsp_setup

--- lua/lsp/setup.lua
+++ lua/lsp/setup.lua
@@ -1,7 +1,7 @@
 return function()
 	require('lsp.lua').lsp_setup()
 	require('lsp.go').lsp_setup()
-	require('lsp.go').golangcilsp_setup()
+	-- require('lsp.go').golangcilsp_setup()
 	require('lsp.typescript').lsp_setup()
 	require('lsp.yaml').lsp_setup()
 	require('lsp.vim').lsp_setup()
